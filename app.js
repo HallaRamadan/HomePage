5db6a4567f9694f1aaa1ae2b210c1c1873253816
HallaRamadan <s193731@>
2023-02-20 15:58:45 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+import {Info} from "./Checkout_List";
+
+export default function List() {
+    const listItems = Info.map(name =>
+        <li
+            key={}>
+            <p>
+            </p>
+        </li>
+    );
+
+    return ();
+}
+
+
+
